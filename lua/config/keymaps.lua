ec77d294540b5bb33e8057fd148d82bfedf08233
Moises <moisescastellano12312@gmail.com>
2025-07-13 17:37:39 -0400

    misc(general): optimization

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,9 +11,14 @@ vim.keymap.set('n', 'Q', '<nop>')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- Remap for dealing with word wrap only in specific file types
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'text', 'markdown'},
+  callback = function()
+    vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+    vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+  end
+})
 
 vim.keymap.set('n', '<leader>y', '"+y')
 vim.keymap.set('v', '<leader>y', '"+y')
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.autoformat = false
 
+vim.opt.laststatus = 3
+
 -- [[ Setting options ]]
 -- See `:help vim.o`
 vim.o.hlsearch = false
